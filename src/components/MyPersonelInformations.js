f0563e0d0ebf8f5dc55d823edf77d98be46e319d
Mustafa ALKAN <mustafaalkan64@gmail.com>
2021-01-21 00:52:43 +0300

    user personel informations page design

--- src/components/MyPersonelInformations.js
+++ src/components/MyPersonelInformations.js
@@ -1,11 +1,206 @@
-import React, { Component } from 'react'
-export default class MyPersonelInformations extends Component {
-    render() {
-        return (
-            <div>
-                Kişisel Bilgilerim
+import React, { useState, useEffect } from "react";
+import {
+  Form,
+  Input,
+  Button,
+  Row,
+  Col,
+  DatePicker,
+  Select,
+  Typography,
+  ConfigProvider,
+} from "antd";
+import { UserOutlined } from "@ant-design/icons";
+import moment from "moment";
+import "moment/locale/tr";
+import locale from "antd/lib/locale/tr_TR";
+const { Option } = Select;
+const { Title } = Typography;
+
+const MyPersonelInformations = () => {
+  const [form] = Form.useForm();
+  const [, forceUpdate] = useState({}); // To disable submit button at the beginning.
+  const dateFormat = "DD.MM.YYYY";
+
+  const style = { padding: "8px 0" };
+  const cities = [
+    { id: 1, cityname: "İzmir" },
+    { id: 2, cityname: "Ankara" },
+  ];
+  console.log(cities);
+
+  const cityOptions = [];
+  const districtOptions = [];
+  cities.map((city) => {
+    cityOptions.push(<Option key={city.id}>{city.cityname}</Option>);
+  });
+
+  useEffect(() => {
+    forceUpdate({});
+  }, []);
+
+  const handleCityChange = (value) => {
+    console.log(`Selected: ${value}`);
+  };
+
+  const handleDistrictChange = (value) => {
+    console.log(`Selected: ${value}`);
+  };
+
+  const onBirthdayChange = (date, dateString) => {};
+
+  const onFinish = (values) => {
+    console.log("Finish:", values);
+  };
+  return (
+    <div>
+      <Row>
+        <Col span={23}>
+          <Title style={{ textAlign: "center" }} level={2}>
+            Kişisel Bilgilerim
+          </Title>
+        </Col>
+      </Row>
+
+      <Form
+        form={form}
+        name="horizontal_login"
+        layout="horizontal"
+        onFinish={onFinish}
+      >
+        <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
+          <Col className="gutter-row" span={8} offset={4}>
+            <div style={style}>
+              <Form.Item
+                name="name"
+                rules={[
+                  {
+                    required: true,
+                    message: "Lütfen İsminizi Giriniz!",
+                  },
+                ]}
+              >
+                <Input
+                  prefix={<UserOutlined className="site-form-item-icon" />}
+                  placeholder="İsim"
+                />
+              </Form.Item>
             </div>
-        )
-    }
-}
-     
+          </Col>
+          <Col className="gutter-row" span={8}>
+            <div style={style}>
+              <Form.Item
+                name="surname"
+                rules={[
+                  {
+                    required: true,
+                    message: "Lütfen Soyisminizi Giriniz!",
+                  },
+                ]}
+              >
+                <Input
+                  prefix={<UserOutlined className="site-form-item-icon" />}
+                  placeholder="Soyisim"
+                />
+              </Form.Item>
+            </div>
+          </Col>
+        </Row>
+
+        <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
+          <Col className="gutter-row" span={8} offset={4}>
+            <div style={style}>
+              <Form.Item
+                name="birthDay"
+                rules={[
+                  {
+                    required: true,
+                    message: "Lütfen Doğum Tarihinizi Giriniz!",
+                  },
+                ]}
+              >
+                <ConfigProvider locale={locale}>
+                  <DatePicker
+                    format={dateFormat}
+                    onChange={onBirthdayChange}
+                    placeholder="Doğum Tarihiniz"
+                    style={{ width: "100%" }}
+                  />
+                </ConfigProvider>
+              </Form.Item>
+            </div>
+          </Col>
+          <Col className="gutter-row" span={8}>
+            <div style={style}>
+              <Form.Item name="email">
+                <Input placeholder="email" disabled="disabled" />
+              </Form.Item>
+            </div>
+          </Col>
+        </Row>
+
+        <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
+          <Col className="gutter-row" span={8} offset={4}>
+            <div style={style}>
+              <Form.Item
+                name="city"
+                rules={[
+                  {
+                    required: true,
+                    message: "Lütfen İl Giriniz!",
+                  },
+                ]}
+              >
+                <Select
+                  size={"default"}
+                  onChange={handleCityChange}
+                  style={{ width: "100%" }}
+                >
+                  {cityOptions}
+                </Select>
+              </Form.Item>
+            </div>
+          </Col>
+          <Col className="gutter-row" span={8}>
+            <div style={style}>
+              <Form.Item
+                name="district"
+                rules={[
+                  {
+                    required: true,
+                    message: "Lütfen İlçe Giriniz!",
+                  },
+                ]}
+              >
+                <Select
+                  size={"default"}
+                  onChange={handleDistrictChange}
+                  style={{ width: "100%" }}
+                >
+                  {districtOptions}
+                </Select>
+              </Form.Item>
+            </div>
+          </Col>
+        </Row>
+
+        <Form.Item shouldUpdate={true}>
+          {() => (
+            <Button
+              type="primary"
+              htmlType="submit"
+              disabled={
+                !form.isFieldsTouched(true) ||
+                !!form.getFieldsError().filter(({ errors }) => errors.length)
+                  .length
+              }
+            >
+              Log in
+            </Button>
+          )}
+        </Form.Item>
+      </Form>
+    </div>
+  );
+};
+export default MyPersonelInformations;
--- src/components/UserLayout.js
+++ src/components/UserLayout.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect, useContext} from 'react';
+import React, { useState, useEffect, useContext } from "react";
 import { Route, Switch } from "react-router-dom";
 import UserContext from "./../contexts/UserContext";
 import MyActiveAppointments from "./myActiveAppointments";
@@ -10,70 +10,95 @@ import UserLogin from "./UserLogin";
 import NoMatch from "./NoMatch";
 import Home from "./UserHome";
 import SideNav from "./SideNav";
-import { Layout } from 'antd';
-import {
-    MenuUnfoldOutlined,
-    MenuFoldOutlined
-  } from '@ant-design/icons';
-const { Header, Sider, Content} = Layout;
+import { Layout } from "antd";
+import { MenuUnfoldOutlined, MenuFoldOutlined } from "@ant-design/icons";
+const { Header, Sider, Content } = Layout;
 
 export default function UserLayout() {
-
   const { isLoggedIn } = useContext(UserContext);
+  const token = localStorage.getItem("auth_token");
 
-    const handleToggle = (event) => {
-        event.preventDefault();
-        collapse1 ? setCollapse(false) : setCollapse(true);
-    }
+  const handleToggle = (event) => {
+    event.preventDefault();
+    collapse1 ? setCollapse(false) : setCollapse(true);
+  };
 
-    const [collapse1, setCollapse] = useState(false);
-    useEffect(() => {
-        window.innerWidth <= 760 ? setCollapse(true) : setCollapse(false);
-      }, []);
+  const [collapse1, setCollapse] = useState(false);
+  useEffect(() => {
+    window.innerWidth <= 760 ? setCollapse(true) : setCollapse(false);
+  }, []);
 
-
-    return (
-        <div>
-          <Switch>
-                <Route exact path='/login' component={UserLogin} />
-                <Route exact path='/signup' component={UserRegister} />
-                <Route path='/' render={() => (
-                    isLoggedIn ? 
-                    (
-                      <Layout>
-                      <Sider trigger={null} collapsible collapsed={collapse1}>
-                        <SideNav />
-                      </Sider>
-                      <Layout>
-                        <Header className="siteLayoutBackground" style={{padding: 0, background: "#001529"}}>
-                                  {React.createElement(collapse1 ? MenuUnfoldOutlined : MenuFoldOutlined, {
-                                      className: 'trigger',
-                                      onClick: handleToggle,
-                                      style: {color: "#fff"}
-                                  })}
-                        </Header>
-                          <Content style={{margin: '24px 16px', padding: 24, minHeight: "calc(100vh - 114px)", background: "#fff"}}>
-                            <Switch>
-                                <Route path="/" exact component={Home} />
-                                {/* <Route path="/myAppointments/:type" component={MyAppointments} /> */}
-                                <Route path="/myActiveAppointments" component={MyActiveAppointments} />
-                                <Route path="/MyCanceledAppointments" component={MyCanceledAppointments} />
-                                <Route path="/createAppointment" component={CreateAppointment} />
-                                <Route path="/myPersonelInformations" component={MyPersonelInformations} />
-                                <Route path="*">
-                                  <NoMatch />
-                                </Route>
-                            </Switch>
-                          </Content>
-                      </Layout>
-                    </Layout>
-                  ) : 
-                  (
-                    <UserLogin />
-                )
-                )} />
-            </Switch>
-          
-        </div>
-    )
+  return (
+    <div>
+      <Switch>
+        <Route exact path="/login" component={UserLogin} />
+        <Route exact path="/signup" component={UserRegister} />
+        <Route
+          path="/"
+          render={() =>
+            isLoggedIn || token != null ? (
+              <Layout>
+                <Sider trigger={null} collapsible collapsed={collapse1}>
+                  <SideNav />
+                </Sider>
+                <Layout>
+                  <Header
+                    className="siteLayoutBackground"
+                    style={{ padding: 0, background: "#001529" }}
+                  >
+                    {React.createElement(
+                      collapse1 ? MenuUnfoldOutlined : MenuFoldOutlined,
+                      {
+                        className: "trigger",
+                        onClick: handleToggle,
+                        style: { color: "#fff" },
+                      }
+                    )}
+                  </Header>
+                  <Content
+                    style={{
+                      margin: "24px 16px",
+                      padding: 24,
+                      minHeight: "calc(100vh - 114px)",
+                      background: "#fff",
+                    }}
+                  >
+                    <Switch>
+                      <Route path="/" exact component={Home} />
+                      <Route
+                        path="/myPersonelInformations"
+                        component={MyPersonelInformations}
+                      />
+                      {/* <Route path="/myPersonelInformations/:id" component={MyPersonelInformations} /> */}
+                      <Route
+                        path="/myActiveAppointments"
+                        component={MyActiveAppointments}
+                      />
+                      <Route
+                        path="/MyCanceledAppointments"
+                        component={MyCanceledAppointments}
+                      />
+                      <Route
+                        path="/createAppointment"
+                        component={CreateAppointment}
+                      />
+                      <Route
+                        path="/myPersonelInformations"
+                        component={MyPersonelInformations}
+                      />
+                      <Route path="*">
+                        <NoMatch />
+                      </Route>
+                    </Switch>
+                  </Content>
+                </Layout>
+              </Layout>
+            ) : (
+              <UserLogin />
+            )
+          }
+        />
+      </Switch>
+    </div>
+  );
 }
