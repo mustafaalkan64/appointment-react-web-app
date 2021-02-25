68de4c0146d450f76310a84b554a498628433740
mustafaalkan64 <mustafaalkan64@gmail.com>
2021-02-25 01:28:29 -0800

    CreateAppointment sayfası tasarımı düzenlendi

--- src/components/Shop/CreateAppointment.js
+++ src/components/Shop/CreateAppointment.js
@@ -4,19 +4,15 @@ import UserContext from "../../contexts/UserContext";
 import BreadCrumbContext from "../../contexts/BreadcrumbContext";
 import {
   Form,
-  Input,
   Button,
   Row,
   Col,
-  DatePicker,
   TimePicker,
   Select,
   Typography,
-  ConfigProvider,
   message,
 } from "antd";
 import API from "../../api";
-import { UserOutlined } from "@ant-design/icons";
 import "moment/locale/tr";
 import locale from "antd/lib/locale/tr_TR";
 import moment from "moment";
@@ -33,19 +29,26 @@ const CreateAppointment = () => {
   } = useContext(BreadCrumbContext);
   const history = useHistory();
   const [, forceUpdate] = useState({}); // To disable submit button at the beginning.
-  const [loading, setLoading] = useState([]);
-  const [userBirthday, setUserBirthday] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [startTime, setStartTime] = useState("");
+  const [endTime, setEndTime] = useState("");
+  const [timeRanges, setTimeRanges] = useState([]);
   const [size, setSize] = React.useState("default");
   const dateFormat = "DD.MM.YYYY";
   const { Option } = Select;
   const format = "HH:mm";
   const { RangePicker } = TimePicker;
+  const [componentSize, setComponentSize] = useState("default");
 
   const style = { padding: "8px 0" };
 
-  const setCurrenUser = (userForm) => {
+  const onFormLayoutChange = ({ size }) => {
+    setComponentSize(size);
+  };
+
+  const createAppointment = (createAppointmentForm) => {
     setLoading(true);
-    API.put(`user/updateUser`, userForm, {
+    API.post(`shop/createAppointment`, createAppointmentForm, {
       headers: {
         "Content-Type": "application/json",
         Authorization: `Bearer ${token}`,
@@ -68,47 +71,9 @@ const CreateAppointment = () => {
       });
   };
 
-  const getCurrentUser = async () => {
-    setLoading(true);
-    await API.get(`user/currentUser?`, {
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${token}`,
-      },
-    })
-      .then((res) => {
-        setLoading(false);
-        var d = new Date(res.data.birthday);
-        var birthdaystring = d.toLocaleString("tr-TR").substring(0, 10);
-        setUserBirthday(birthdaystring);
-        form.setFieldsValue({
-          name: res.data.firstName,
-          surname: res.data.lastName,
-          email: res.data.email,
-          birthDay: birthdaystring,
-          city: res.data.cityId,
-          district: res.data.districtId,
-          zone: res.data.zoneId,
-          username: res.data.username,
-        });
-        console.log(userBirthday);
-      })
-      .catch((error) => {
-        if (error.response.status === 401) {
-          history.push("/login");
-          message.error("Bu İşlemi Yapmaya Yetkiniz Yok!");
-        } else {
-          message.error(
-            "Kişisel Bilgileri Getirme Esnasında Hata ile Karşılaşıldı!"
-          );
-        }
-      });
-  };
-
   useEffect(() => {
     forceUpdate({});
 
-    getCurrentUser();
     setFirstBreadcrumb("Anasayfa");
     setSecondBreadcrumb("Randevular");
     setLastBreadcrumb("Randevu Oluştur");
@@ -118,29 +83,36 @@ const CreateAppointment = () => {
     console.log(`Selected: ${value}`);
   }
 
-  function onTimeChange(time, timeString) {
-    console.log(time, timeString);
+  function onStartTimeChange(time, timeString) {
+    setStartTime(time);
+  }
+
+  function onEndTimeChange(time, timeString) {
+    setEndTime(time);
+  }
+
+  function onTimeRangeChange(time, timeString) {
+    setTimeRanges(time);
   }
 
   const onFinish = (values) => {
-    const userForm = {
-      FirstName: values.name,
-      LastName: values.surname,
-      CityId: values.city,
-      DistrictId: values.district,
-      ZoneId: values.zone,
-      BirthDay: values.birthDay,
-      Username: values.username,
+    const createAppointmentForm = {
+      WeekDays: values.weekdays,
+      AppointmentPeriod: values.appointmentPeriod,
+      StartTime: startTime,
+      EndTime: endTime,
+      EmptyTimeRange: timeRanges,
+      AppointmentLong: values.appointmentLong,
     };
-    setCurrenUser(userForm);
+    createAppointment(createAppointmentForm);
   };
 
   const layout = {
     labelCol: {
-      span: 6,
+      span: 8,
     },
     wrapperCol: {
-      span: 18,
+      span: 10,
     },
   };
 
@@ -156,176 +128,164 @@ const CreateAppointment = () => {
 
       <Form
         form={form}
-        name="horizontal_login"
+        {...layout}
+        name="createAppointmentForm"
         layout="horizontal"
+        initialValues={{
+          size: componentSize,
+        }}
+        onValuesChange={onFormLayoutChange}
+        size={componentSize}
         onFinish={onFinish}
       >
-        <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
-          <Col className="gutter-row" span={12} offset={4}>
-            <div style={style}>
-              <Form.Item
-                label="Hizmet Verilen Günler"
-                name="name"
-                labelCol={{ span: 8 }}
-                wrapperCol={{ span: 18 }}
-                rules={[
-                  {
-                    required: true,
-                    message: "Lütfen Boş Bırakmayınız!",
-                  },
-                ]}
-              >
-                <Select
-                  mode="tags"
-                  size={size}
-                  placeholder="Seçiniz"
-                  defaultValue={[]}
-                  onChange={handleChange}
-                  style={{ width: "100%" }}
-                >
-                  <Option key="Pzt">Pazartesi</Option>
-                  <Option key="Salı">Salı</Option>
-                  <Option key="Çar">Çaşamba</Option>
-                  <Option key="Per">Perşembe</Option>
-                  <Option key="Cum">Cuma</Option>
-                  <Option key="Cmt">Cumartesi</Option>
-                  <Option key="Pz">Pazar</Option>
-                </Select>
-              </Form.Item>
-            </div>
-          </Col>
-        </Row>
+        <Form.Item
+          label="Hizmet Verilen Günler"
+          name="weekdays"
+          rules={[
+            {
+              required: true,
+              message: "Lütfen Boş Bırakmayınız!",
+            },
+          ]}
+        >
+          <Select
+            mode="tags"
+            size={size}
+            placeholder="Seçiniz"
+            defaultValue={[]}
+            onChange={handleChange}
+            style={{ width: "100%" }}
+          >
+            <Option key="1">Pazartesi</Option>
+            <Option key="2">Salı</Option>
+            <Option key="3">Çaşamba</Option>
+            <Option key="4">Perşembe</Option>
+            <Option key="5">Cuma</Option>
+            <Option key="6">Cumartesi</Option>
+            <Option key="7">Pazar</Option>
+          </Select>
+        </Form.Item>
 
-        <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
-          <Col className="gutter-row" span={12} offset={4}>
-            <div style={style}>
-              <Form.Item
-                label="Randevu Period (Dakika)"
-                name="appointmentRange"
-                labelCol={{ span: 8 }}
-                wrapperCol={{ span: 18 }}
-                rules={[
-                  {
-                    required: true,
-                    message: "Lütfen Boş Bırakmayınız!",
-                  },
-                ]}
-              >
-                <Select
-                  size={size}
-                  placeholder="Seçiniz"
-                  defaultValue={[]}
-                  onChange={handleChange}
-                  style={{ width: "100%" }}
-                >
-                  <Option key="15">15</Option>
-                  <Option key="30">30</Option>
-                  <Option key="60">60</Option>
-                  <Option key="90">90</Option>
-                </Select>
-              </Form.Item>
-            </div>
-          </Col>
-        </Row>
+        <Form.Item
+          label="Randevu Period (Dakika)"
+          name="appointmentPeriod"
+          rules={[
+            {
+              required: true,
+              message: "Lütfen Boş Bırakmayınız!",
+            },
+          ]}
+        >
+          <Select
+            size={size}
+            placeholder="Seçiniz"
+            defaultValue={[]}
+            onChange={handleChange}
+            style={{ width: "100%" }}
+          >
+            <Option key="15">15</Option>
+            <Option key="30">30</Option>
+            <Option key="60">60</Option>
+            <Option key="90">90</Option>
+          </Select>
+        </Form.Item>
 
-        <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
-          <Col className="gutter-row" span={12} offset={4}>
-            <div style={style}>
-              <Form.Item
-                label="Açılış Saati"
-                name="startTime"
-                labelCol={{ span: 8 }}
-                wrapperCol={{ span: 18 }}
-                rules={[
-                  {
-                    required: true,
-                    message: "Lütfen Saat Giriniz!",
-                  },
-                ]}
-              >
-                <TimePicker
-                  defaultValue={moment("00:00", format)}
-                  format={format}
-                />
-              </Form.Item>
-            </div>
-          </Col>
-        </Row>
+        <Form.Item
+          label="Açılış Saati"
+          name="startTime"
+          rules={[
+            {
+              required: true,
+              message: "Lütfen Saat Giriniz!",
+            },
+          ]}
+        >
+          <TimePicker
+            onChange={onStartTimeChange}
+            defaultValue={moment("00:00", format)}
+            format={format}
+          />
+        </Form.Item>
 
-        <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
-          <Col className="gutter-row" span={12} offset={4}>
-            <div style={style}>
-              <Form.Item
-                label="Kapanış Saati"
-                name="endTime"
-                labelCol={{ span: 8 }}
-                wrapperCol={{ span: 18 }}
-                rules={[
-                  {
-                    required: true,
-                    message: "Lütfen Saat Giriniz!",
-                  },
-                ]}
-              >
-                <TimePicker
-                  defaultValue={moment("00:00", format)}
-                  format={format}
-                />
-              </Form.Item>
-            </div>
-          </Col>
-        </Row>
+        <Form.Item
+          label="Kapanış Saati"
+          name="endTime"
+          rules={[
+            {
+              required: true,
+              message: "Lütfen Saat Giriniz!",
+            },
+          ]}
+        >
+          <TimePicker
+            onChange={onEndTimeChange}
+            defaultValue={moment("00:00", format)}
+            format={format}
+          />
+        </Form.Item>
 
-        <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
-          <Col className="gutter-row" span={12} offset={4}>
-            <div style={style}>
-              <Form.Item
-                label="Hizmet Verilmeyen Zaman Dilimi"
-                name="unUsedTime"
-                labelCol={{ span: 8 }}
-                wrapperCol={{ span: 18 }}
-                rules={[
-                  {
-                    required: true,
-                    message: "Lütfen Saat Giriniz!",
-                  },
-                ]}
-              >
-                <RangePicker
-                  format={format}
-                  defaultValue={moment("00:00", format)}
-                />
-              </Form.Item>
-            </div>
-          </Col>
-        </Row>
+        <Form.Item
+          label="Hizmet Verilmeyen Saat Aralığı"
+          name="emptyTimeRange"
+          labelCol={{ span: 8 }}
+          wrapperCol={{ span: 18 }}
+          rules={[
+            {
+              required: true,
+              message: "Lütfen Saat Giriniz!",
+            },
+          ]}
+        >
+          <RangePicker
+            format={format}
+            onChange={onTimeRangeChange}
+            defaultValue={moment("00:00", format)}
+          />
+        </Form.Item>
 
-        <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
-          <Col className="gutter-row" span={12} offset={4}>
-            <div style={style}>
-              <Form.Item
-                wrapperCol={{ ...layout.wrapperCol, offset: 8 }}
-                shouldUpdate={true}
-              >
-                {() => (
-                  <Button
-                    type="primary"
-                    loading={loading}
-                    htmlType="submit"
-                    style={{ width: "100%" }}
-                    disabled={
-                      !!form
-                        .getFieldsError()
-                        .filter(({ errors }) => errors.length).length
-                    }
-                  >
-                    Kaydet
-                  </Button>
-                )}
-              </Form.Item>
-            </div>
-          </Col>
-        </Row>
+        <Form.Item
+          label="Hizmet Süresi"
+          name="appointmentLong"
+          rules={[
+            {
+              required: true,
+              message: "Lütfen Boş Bırakmayınız!",
+            },
+          ]}
+        >
+          <Select
+            size={size}
+            placeholder="Seçiniz"
+            defaultValue={[]}
+            onChange={handleChange}
+            style={{ width: "100%" }}
+          >
+            <Option key="15">15 Gün</Option>
+            <Option key="30">1 Ay</Option>
+            <Option key="60">2 Ay</Option>
+            <Option key="90">3 Ay</Option>
+            <Option key="180">6 Ay</Option>
+          </Select>
+        </Form.Item>
+
+        <Form.Item
+          wrapperCol={{ ...layout.wrapperCol, offset: 8 }}
+          shouldUpdate={true}
+        >
+          {() => (
+            <Button
+              type="primary"
+              loading={loading}
+              htmlType="submit"
+              disabled={
+                !!form.getFieldsError().filter(({ errors }) => errors.length)
+                  .length
+              }
+            >
+              Oluştur
+            </Button>
+          )}
+        </Form.Item>
       </Form>
     </div>
   );
