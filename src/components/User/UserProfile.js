e4985fb8fb10a5505de25a1e5c0e5d750e215b54
mustafaalkan64 <mustafaalkan64@gmail.com>
2021-03-09 11:20:02 -0800

    User Profile page css modified

--- src/components/User/UserProfile.js
+++ src/components/User/UserProfile.js
@@ -10,18 +10,17 @@ import {
   Col,
   DatePicker,
   Select,
-  Typography,
   ConfigProvider,
   message,
   Spin,
+  Card,
 } from "antd";
 import API from "../../api";
 import { UserOutlined } from "@ant-design/icons";
 import "moment/locale/tr";
 import locale from "antd/lib/locale/tr_TR";
 import moment from "moment";
-
-const { Title } = Typography;
+import { cardStyle, headStyle } from "../../assets/styles/styles";
 
 const UserProfile = () => {
   const [form] = Form.useForm();
@@ -208,260 +207,269 @@ const UserProfile = () => {
 
   return (
     <div>
-      <Row>
-        <Col span={23}>
-          <Title style={{ textAlign: "center" }} level={2}>
-            Profil Bilgilerim
-          </Title>
-        </Col>
-      </Row>
       <Spin spinning={loading} delay={500}>
-        <Form
-          form={form}
-          name="horizontal_login"
-          layout="horizontal"
-          onFinish={onFinish}
-        >
-          <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
-            <Col className="gutter-row" span={12} offset={4}>
-              <div style={style}>
-                <Form.Item
-                  label="İsim"
-                  name="name"
-                  labelCol={{ span: 6 }}
-                  wrapperCol={{ span: 18 }}
-                  rules={[
-                    {
-                      required: true,
-                      message: "Lütfen İsminizi Giriniz!",
-                    },
-                  ]}
-                >
-                  <Input
-                    prefix={<UserOutlined className="site-form-item-icon" />}
-                    placeholder="İsim"
-                  />
-                </Form.Item>
-              </div>
-            </Col>
-          </Row>
+        <Row>
+          <Col span={16} offset={4}>
+            <Card
+              title="Kullanıcı Profili"
+              hoverable
+              bordered={true}
+              style={cardStyle}
+              headStyle={headStyle}
+            >
+              <Form
+                form={form}
+                name="horizontal_login"
+                layout="horizontal"
+                onFinish={onFinish}
+              >
+                <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
+                  <Col className="gutter-row" span={12} offset={4}>
+                    <div style={style}>
+                      <Form.Item
+                        label="İsim"
+                        name="name"
+                        labelCol={{ span: 6 }}
+                        wrapperCol={{ span: 18 }}
+                        rules={[
+                          {
+                            required: true,
+                            message: "Lütfen İsminizi Giriniz!",
+                          },
+                        ]}
+                      >
+                        <Input
+                          prefix={
+                            <UserOutlined className="site-form-item-icon" />
+                          }
+                          placeholder="İsim"
+                        />
+                      </Form.Item>
+                    </div>
+                  </Col>
+                </Row>
 
-          <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
-            <Col className="gutter-row" span={12} offset={4}>
-              <div style={style}>
-                <Form.Item
-                  name="surname"
-                  label="Soyisim"
-                  labelCol={{ span: 6 }}
-                  wrapperCol={{ span: 18 }}
-                  rules={[
-                    {
-                      required: true,
-                      message: "Lütfen Soyisminizi Giriniz!",
-                    },
-                  ]}
-                >
-                  <Input
-                    prefix={<UserOutlined className="site-form-item-icon" />}
-                    placeholder="Soyisim"
-                  />
-                </Form.Item>
-              </div>
-            </Col>
-          </Row>
+                <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
+                  <Col className="gutter-row" span={12} offset={4}>
+                    <div style={style}>
+                      <Form.Item
+                        name="surname"
+                        label="Soyisim"
+                        labelCol={{ span: 6 }}
+                        wrapperCol={{ span: 18 }}
+                        rules={[
+                          {
+                            required: true,
+                            message: "Lütfen Soyisminizi Giriniz!",
+                          },
+                        ]}
+                      >
+                        <Input
+                          prefix={
+                            <UserOutlined className="site-form-item-icon" />
+                          }
+                          placeholder="Soyisim"
+                        />
+                      </Form.Item>
+                    </div>
+                  </Col>
+                </Row>
 
-          <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
-            <Col className="gutter-row" span={12} offset={4}>
-              <div style={style}>
-                <Form.Item
-                  name="birthDay"
-                  label="Doğum Tarihi"
-                  labelCol={{ span: 6 }}
-                  wrapperCol={{ span: 18 }}
-                  rules={[
-                    {
-                      required: true,
-                      message: "Lütfen Doğum Tarihinizi Giriniz!",
-                    },
-                  ]}
-                >
-                  <ConfigProvider locale={locale}>
-                    <DatePicker
-                      format={dateFormat}
-                      defaultValue={moment(userBirthday, dateFormat)}
-                      value={moment(userBirthday, dateFormat)}
-                      onChange={onBirthdayChange}
-                      placeholder="Doğum Tarihiniz"
-                      style={{ width: "100%" }}
-                    />
-                  </ConfigProvider>
-                </Form.Item>
-              </div>
-            </Col>
-          </Row>
+                <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
+                  <Col className="gutter-row" span={12} offset={4}>
+                    <div style={style}>
+                      <Form.Item
+                        name="birthDay"
+                        label="Doğum Tarihi"
+                        labelCol={{ span: 6 }}
+                        wrapperCol={{ span: 18 }}
+                        rules={[
+                          {
+                            required: true,
+                            message: "Lütfen Doğum Tarihinizi Giriniz!",
+                          },
+                        ]}
+                      >
+                        <ConfigProvider locale={locale}>
+                          <DatePicker
+                            format={dateFormat}
+                            defaultValue={moment(userBirthday, dateFormat)}
+                            value={moment(userBirthday, dateFormat)}
+                            onChange={onBirthdayChange}
+                            placeholder="Doğum Tarihiniz"
+                            style={{ width: "100%" }}
+                          />
+                        </ConfigProvider>
+                      </Form.Item>
+                    </div>
+                  </Col>
+                </Row>
 
-          <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
-            <Col className="gutter-row" span={12} offset={4}>
-              <div style={style}>
-                <Form.Item
-                  name="username"
-                  label="Kullanıcı Adı"
-                  labelCol={{ span: 6 }}
-                  wrapperCol={{ span: 18 }}
-                  rules={[
-                    {
-                      required: true,
-                      message: "Lütfen Kullanıcı Adını Giriniz!",
-                    },
-                  ]}
-                >
-                  <Input placeholder="Kullanıcı Adı" />
-                </Form.Item>
-              </div>
-            </Col>
-          </Row>
+                <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
+                  <Col className="gutter-row" span={12} offset={4}>
+                    <div style={style}>
+                      <Form.Item
+                        name="username"
+                        label="Kullanıcı Adı"
+                        labelCol={{ span: 6 }}
+                        wrapperCol={{ span: 18 }}
+                        rules={[
+                          {
+                            required: true,
+                            message: "Lütfen Kullanıcı Adını Giriniz!",
+                          },
+                        ]}
+                      >
+                        <Input placeholder="Kullanıcı Adı" />
+                      </Form.Item>
+                    </div>
+                  </Col>
+                </Row>
 
-          <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
-            <Col className="gutter-row" span={12} offset={4}>
-              <div style={style}>
-                <Form.Item
-                  name="email"
-                  labelCol={{ span: 6 }}
-                  wrapperCol={{ span: 18 }}
-                  label="Email"
-                >
-                  <Input placeholder="email" disabled="disabled" />
-                </Form.Item>
-              </div>
-            </Col>
-          </Row>
+                <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
+                  <Col className="gutter-row" span={12} offset={4}>
+                    <div style={style}>
+                      <Form.Item
+                        name="email"
+                        labelCol={{ span: 6 }}
+                        wrapperCol={{ span: 18 }}
+                        label="Email"
+                      >
+                        <Input placeholder="email" disabled="disabled" />
+                      </Form.Item>
+                    </div>
+                  </Col>
+                </Row>
 
-          <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
-            <Col className="gutter-row" span={12} offset={4}>
-              <div style={style}>
-                <Form.Item
-                  name="city"
-                  label="İl"
-                  labelCol={{ span: 6 }}
-                  wrapperCol={{ span: 20 }}
-                  rules={[
-                    {
-                      required: true,
-                      message: "Lütfen İl Giriniz!",
-                    },
-                  ]}
-                >
-                  <Select
-                    size={"default"}
-                    onChange={handleCityChange}
-                    style={{ width: "100%" }}
-                  >
-                    {cities.map((city, key) => {
-                      return (
-                        <Option value={city.id} key={city.id}>
-                          {city.cityName}
-                        </Option>
-                      );
-                    })}
-                  </Select>
-                </Form.Item>
-              </div>
-            </Col>
-          </Row>
+                <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
+                  <Col className="gutter-row" span={12} offset={4}>
+                    <div style={style}>
+                      <Form.Item
+                        name="city"
+                        label="İl"
+                        labelCol={{ span: 6 }}
+                        wrapperCol={{ span: 20 }}
+                        rules={[
+                          {
+                            required: true,
+                            message: "Lütfen İl Giriniz!",
+                          },
+                        ]}
+                      >
+                        <Select
+                          size={"default"}
+                          onChange={handleCityChange}
+                          style={{ width: "100%" }}
+                        >
+                          {cities.map((city, key) => {
+                            return (
+                              <Option value={city.id} key={city.id}>
+                                {city.cityName}
+                              </Option>
+                            );
+                          })}
+                        </Select>
+                      </Form.Item>
+                    </div>
+                  </Col>
+                </Row>
 
-          <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
-            <Col className="gutter-row" span={12} offset={4}>
-              <div style={style}>
-                <Form.Item
-                  name="district"
-                  label="İlçe"
-                  labelCol={{ span: 6 }}
-                  wrapperCol={{ span: 20 }}
-                  rules={[
-                    {
-                      required: true,
-                      message: "Lütfen İlçe Giriniz!",
-                    },
-                  ]}
-                >
-                  <Select
-                    size={"default"}
-                    onChange={handleDistrictChange}
-                    style={{ width: "100%" }}
-                  >
-                    {districts.map((district, key) => {
-                      return (
-                        <Option value={district.id} key={district.id}>
-                          {district.districtName}
-                        </Option>
-                      );
-                    })}
-                  </Select>
-                </Form.Item>
-              </div>
-            </Col>
-          </Row>
+                <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
+                  <Col className="gutter-row" span={12} offset={4}>
+                    <div style={style}>
+                      <Form.Item
+                        name="district"
+                        label="İlçe"
+                        labelCol={{ span: 6 }}
+                        wrapperCol={{ span: 20 }}
+                        rules={[
+                          {
+                            required: true,
+                            message: "Lütfen İlçe Giriniz!",
+                          },
+                        ]}
+                      >
+                        <Select
+                          size={"default"}
+                          onChange={handleDistrictChange}
+                          style={{ width: "100%" }}
+                        >
+                          {districts.map((district, key) => {
+                            return (
+                              <Option value={district.id} key={district.id}>
+                                {district.districtName}
+                              </Option>
+                            );
+                          })}
+                        </Select>
+                      </Form.Item>
+                    </div>
+                  </Col>
+                </Row>
 
-          <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
-            <Col className="gutter-row" span={12} offset={4}>
-              <div style={style}>
-                <Form.Item
-                  labelCol={{ span: 6 }}
-                  wrapperCol={{ span: 20 }}
-                  name="zone"
-                  label="Köy/Mahalle"
-                  rules={[
-                    {
-                      required: true,
-                      message: "Lütfen Semt/Mahalle Giriniz!",
-                    },
-                  ]}
-                >
-                  <Select
-                    size={"default"}
-                    onChange={handleZoneChange}
-                    style={{ width: "100%" }}
-                  >
-                    {zones.map((zone, key) => {
-                      return (
-                        <Option value={zone.id} key={zone.id}>
-                          {zone.zoneName}
-                        </Option>
-                      );
-                    })}
-                  </Select>
-                </Form.Item>
-              </div>
-            </Col>
-          </Row>
+                <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
+                  <Col className="gutter-row" span={12} offset={4}>
+                    <div style={style}>
+                      <Form.Item
+                        labelCol={{ span: 6 }}
+                        wrapperCol={{ span: 20 }}
+                        name="zone"
+                        label="Köy/Mahalle"
+                        rules={[
+                          {
+                            required: true,
+                            message: "Lütfen Semt/Mahalle Giriniz!",
+                          },
+                        ]}
+                      >
+                        <Select
+                          size={"default"}
+                          onChange={handleZoneChange}
+                          style={{ width: "100%" }}
+                        >
+                          {zones.map((zone, key) => {
+                            return (
+                              <Option value={zone.id} key={zone.id}>
+                                {zone.zoneName}
+                              </Option>
+                            );
+                          })}
+                        </Select>
+                      </Form.Item>
+                    </div>
+                  </Col>
+                </Row>
 
-          <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
-            <Col className="gutter-row" span={12} offset={4}>
-              <div style={style}>
-                <Form.Item
-                  wrapperCol={{ ...layout.wrapperCol, offset: 6 }}
-                  shouldUpdate={true}
-                >
-                  {() => (
-                    <Button
-                      type="primary"
-                      loading={loading}
-                      htmlType="submit"
-                      style={{ width: "100%" }}
-                      disabled={
-                        !!form
-                          .getFieldsError()
-                          .filter(({ errors }) => errors.length).length
-                      }
-                    >
-                      Kaydet
-                    </Button>
-                  )}
-                </Form.Item>
-              </div>
-            </Col>
-          </Row>
-        </Form>
+                <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
+                  <Col className="gutter-row" span={12} offset={4}>
+                    <div style={style}>
+                      <Form.Item
+                        wrapperCol={{ ...layout.wrapperCol, offset: 6 }}
+                        shouldUpdate={true}
+                      >
+                        {() => (
+                          <Button
+                            type="primary"
+                            loading={loading}
+                            htmlType="submit"
+                            style={{ width: "100%" }}
+                            disabled={
+                              !!form
+                                .getFieldsError()
+                                .filter(({ errors }) => errors.length).length
+                            }
+                          >
+                            Kaydet
+                          </Button>
+                        )}
+                      </Form.Item>
+                    </div>
+                  </Col>
+                </Row>
+              </Form>
+            </Card>
+          </Col>
+        </Row>
       </Spin>
     </div>
   );
