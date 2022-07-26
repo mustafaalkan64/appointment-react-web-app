29b21308800930cfd1a5d261250d7faf1aa38078
mustafaalkan64 <mustafaalkan64@gmail.com>
2022-07-27 02:06:18 +0300

    converted local to cloud url

--- src/constUrls.js
+++ src/constUrls.js
@@ -1,3 +1,4 @@
 //const appointmentHub = "https://localhost:5001/appointmentHub";
 export const appointmentHub = "https://localhost:44373/appointmentHub";
-export const imageUrlDirectory = "https://localhost:44316/wwwroot/";
+//export const imageUrlDirectory = "https://localhost:44316/wwwroot/";
+export const imageUrlDirectory = "https://appointmentapi20220719000004.azurewebsites.net/wwwroot/";
